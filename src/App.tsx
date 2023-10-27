951bc1204b6ebdaf42103ca9ebb1b31b40a53464
Vanshika Dargan <vanshikadargan.vd@gmail.com>
2023-10-28 00:55:07 +0530

    React Hooks code added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
-import Heading from './components/Heading'
-import {Section} from './components/Section'
-import Counter from './components/Counter'
-import { useState } from 'react'
-import List from './components/List'
+import {useState,useEffect,useCallback,MouseEvent,KeyboardEvent,useMemo,useRef} from 'react'
+
+interface User{
+  id:number,
+  username:string
+}
+type fibFunc=(n:number)=>number
+
+const fib:fibFunc=(n:number)=>{
+  if(n<2) return n
+  return fib(n-1)+fib(n-2)
+}
+
+const myBigNumber=35
+
+
 const App = () => {
-  const [count,setCount]=useState<number>(1)
+
+  // const inputRef=useRef<HTMLInputElement>(null!);
+  // useState<number>(0) useState<number | null>(null) useState<User | null>(null) useState<User[]>([]) useState<User>({} as User)
+    const [count,setCount]=
+    useState<number>(0)
+    const [users,setUsers]=useState<User[] | null>(null)
+    
+    const inputRef=useRef<HTMLInputElement>(null);
+
+    console.log(inputRef?.current)
+    console.log(inputRef?.current?.value)
+    // In React 18+ there is mounting,unmounting, and then remounting
+    useEffect(()=>{
+    console.log('Mounting')
+    console.log('Users:',users)
+    return ()=>console.log('Unmounting')
+    },[users])
+
+    // useCallback is used to memoize large functions
+    // const addTwo=useCallback((e:MouseEvent<HTMLButtonElement> |
+    //   KeyboardEvent<HTMLButtonElement>):void =>setCount(prev=>prev+2),[])
+
+    const addTwo=useCallback(():void=>setCount(prev=>prev+2),[])
+
+
+    // to memoize a value we could useMemoize, we can use it to store expensive calculation results
+    const result=useMemo<number>(()=>fib(myBigNumber),[myBigNumber])
   return (
-    <>
-    <Heading title={"Hello"}/>
-    <Section title={"MyTitle"}>This is my Section.</Section>
-    <Counter setCount={setCount}>Count is {count}</Counter>
-    <List items={['Coffee','Tea','Code']} render={(item:string)=><span className='gold bold'>{item}</span>}/>
-    </>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text"/>
+    </div>
   )
 }
 
